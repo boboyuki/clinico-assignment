9297ffa0b7a78ef12e1d05204cb0c1a5463ccfa4
PG <frankpg267@gmail.com>
2024-11-17 19:40:08 +0800

    chore: add eslint and prettier settings, add style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: false,
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
